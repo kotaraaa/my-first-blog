b6ac01b22ad9b7c29994a49a29ec8d25ad3c792f
kotara <kotara-98@bk.ru>
2017-06-04 19:44:30 +0300

    new edit

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
+
+def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
